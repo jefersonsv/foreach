b7c1a989880de13ee8d98e47d852cf60b00b52ef
Jeferson Tenorio <jefersonsv@gmail.com>
2018-08-29 10:40:50 -0300

    fix deploy bat

--- deploy-src.cmd
+++ deploy-src.cmd
@@ -1,4 +1,4 @@
-SET app=ForeachUtility
+SET app=Foreach
 rd /S /Q .\src\%app%\bin\Release
 dotnet build -c Release .\src\%app%\%app%.csproj
 nuget push .\src\%app%\bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
